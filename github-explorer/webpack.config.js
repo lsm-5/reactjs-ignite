508e3ba5d674f91950e7135b1e267fe423d9ff1e
lsm-5 <lsm5@cin.ufpe.br>
2021-03-08 22:22:24 -0300

    Aula05: Servindo HTML estático

--- github-explorer/webpack.config.js
+++ github-explorer/webpack.config.js
@@ -1,22 +1,30 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.jsx', '.js'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: 'babel-loader',
       }
-    ]
+    ],
   }
 };
