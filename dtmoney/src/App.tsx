5f4b35741f1c9bac81b5a516a4d108b5e696d3e7
lsm-5 <lsm5@cin.ufpe.br>
2021-05-16 20:41:04 -0300

    capítulo 2

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,11 +1,38 @@
+import { useCallback, useState } from 'react'
+
+import { Dashboard } from "./components/Dashboard";
+import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
+import { TransactionsProvider } from './hooks/useTransactions';
 import { GlobalStyle } from "./styles/global";
 
-
 export function App() {
+  const [
+    isNewTransactionModalOpen,
+    setIsNewTransactionModalOpen
+  ] = useState(false);
+
+  const handleOpenNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(true)
+  }, [])
+
+  const handleCloseNewTransactionModal = useCallback(() => {
+    setIsNewTransactionModalOpen(false)
+  }, [])
+
+
   return (
-    <div className="App">
-      <h1>Olá</h1>
-      <GlobalStyle />
-    </div>
+    <TransactionsProvider>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
+      <Dashboard />
+      
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      
+      <GlobalStyle/>
+    </TransactionsProvider>
   );
 }
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from './App';
+
+import { createServer, Model } from 'miragejs'
+
+import { App } from './App';
+
+createServer({
+  models: {
+    transaction: Model
+  },
+
+  seeds(server: { db: { loadData: (arg0: { transactions: { id: number; title: string; type: string; category: string; amount: number; createdAt: Date; }[]; }) => void; }; }) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2021-05-12 15:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2021-05-14 17:00:00')
+        },
+      ]
+    })
+  },
+
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (_: any, request: { requestBody: string; }) => {
+      const data = JSON.parse(request.requestBody);
+
+      return this.schema.create('transaction', data);
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -8,3 +54,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
